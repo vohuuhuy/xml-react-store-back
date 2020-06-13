00b444323557dbd69c59199d446609a108978d3c
vohuy <vhhuy.c3tqcap.a7@gmail.com>
2020-06-13 18:16:17 +0700

    update

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -16,6 +16,42 @@ export class UserResolve {
     }
   }
 
+  @Mutation('updateUser')
+  async updateUser(
+    @Args('input') NguoiDung
+  ) {
+    try {
+      await R.DB.updateUser(JSON.parse(NguoiDung))
+      return 'Cập nhật thông tin thành công'
+    } catch (err) {
+      throw new ApolloError(err)
+    }
+  }
+
+  @Mutation('addUser')
+  async addUser(
+    @Args('input') NguoiDung
+  ) {
+    try {
+      await R.DB.addUser(JSON.parse(NguoiDung))
+      return 'Thêm người dùng thành công'
+    } catch (err) {
+      throw new ApolloError(err)
+    }
+  }
+
+  @Mutation('deleteUsers')
+  async deleteUsers(
+    @Args('TaiKhoans') TaiKhoans
+  ) {
+    try {
+      await R.DB.deleteUsers(TaiKhoans)
+      return 'Xóa thành công'
+    } catch (err) {
+      throw new ApolloError(err)
+    }
+  }
+
   @Query('login')
   async login(@Args() args) {
     try {
@@ -25,6 +61,26 @@ export class UserResolve {
         return JSON.stringify(result)
       }
       return null
+    } catch (error) {
+      throw new ApolloError(error)
+    }
+  }
+
+  @Query('findUser')
+  async findUser (
+    @Args('TaiKhoan') TaiKhoan
+  ) {
+    try {
+      return JSON.stringify(await R.DB.findUser(TaiKhoan))
+    } catch (error) {
+      throw new ApolloError(error)
+    }
+  }
+
+  @Query('findAllUser')
+  async findAllUser () {
+    try {
+      return JSON.stringify(await R.DB.findAllUser())
     } catch (error) {
       throw new ApolloError(error)
     }
--- src/resource/db/index.ts
+++ src/resource/db/index.ts
@@ -3,14 +3,83 @@ import * as xml2json from 'xml2json'
 import * as path from 'path'
 import { PATH_XML } from 'resource/variables'
 
-export const getAll = async () => {
-  console.log(PATH_XML || path.resolve(__dirname, './QLTH.xml').replace('dist', 'src'))
+const header = `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="QLTH.xsl" ?>
+<QLTH
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="QLTH.xsd"
+>`
 
-  const result = fs.readFileSync(PATH_XML || path.resolve(__dirname, './QLTH.xml').replace('dist', 'src'))
+const headerFail = `xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="QLTH.xsd"`
+
+const footer = `</QLTH>`
+
+const pathXML = PATH_XML || path.resolve(__dirname, './QLTH.xml').replace('dist', 'src')
+
+const convertXML = object => (xml2json.toXml(JSON.stringify(object)).replace(headerFail, header) + footer).trim()
+
+export const getAll = async () => {
+  const result = fs.readFileSync(pathXML)
   return JSON.parse(xml2json.toJson(result, { reversible: true }))
 }
 
 export const findUser = async (TaiKhoan) => {
   const { QLTH: { NguoiDung } } = await getAll()
-  return [].concat(NguoiDung)?.find(i => i.TaiKhoan["$t"] === TaiKhoan)
+  return [].concat(NguoiDung)?.find(i => i.TaiKhoan['$t'] === TaiKhoan)
+}
+
+export const findAllUser = async () => {
+  const { QLTH: { NguoiDung } } = await getAll()
+  return [].concat(NguoiDung)
+}
+
+export const addUser = async (input) => {
+  const { QLTH } = await getAll()
+  const users = await findAllUser()
+  if (users.find(user => user.TaiKhoan['$t'] === input.TaiKhoan['$t'])) return 'Tài khoản đã tồn tại'
+  const MaND = 'ND' + `000${parseInt(users?.sort()[users?.length - 1].MaND['$t'].substr(2)) + 1}`.substr(-3)
+  users.push({ MaND: { '$t': MaND }, ...input })
+  const newXML = {
+    ...QLTH,
+    NguoiDung: users
+  }
+  fs.writeFileSync(pathXML, convertXML(newXML))
+  return true
+}
+
+export const updateUser = async (input) => {
+  const { QLTH } = await getAll()
+  const { NguoiDung } = QLTH
+  let newNguoiDung
+  if (NguoiDung?.length) {
+    newNguoiDung = NguoiDung.map(i => {
+      if (i.TaiKhoan['$t'] === input.TaiKhoan['$t']) return ({
+        ...i,
+        ...input
+      })
+      return i
+    })
+  } else if (NguoiDung.TaiKhoan['$t'] === input.TaiKhoan['$t']) {
+    newNguoiDung = {
+      ...NguoiDung,
+      ...input
+    }
+  }
+  const newXML = {
+    ...QLTH,
+    NguoiDung: newNguoiDung
+  }
+  fs.writeFileSync(pathXML, convertXML(newXML))
+  return true
+}
+
+export const deleteUsers = async (TaiKhoans = []) => {
+  const { QLTH } = await getAll()
+  const users = await findAllUser()
+  const newXML = {
+    ...QLTH,
+    NguoiDung: users.filter(user => !TaiKhoans.includes(user.TaiKhoan['$t']))
+  }
+  fs.writeFileSync(pathXML, convertXML(newXML))
+  return true
 }
